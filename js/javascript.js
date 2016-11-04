503ea807a7d84e76b11cecc08116f59918b4ca11
IvanBarr <ivanbar.webdev@gmail.com>
2016-11-03 17:34:51 -0700

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+$(function(){
+  $(".glasses").hover(function(){
+      $(".hover-box").fadeToggle(200);
+  });
+
+    $(".leaf").hover(function(){
+        $(".hover-box2").fadeToggle(200);
+    });
+
+    $(".car").hover(function(){
+        $(".hover-box3").fadeToggle(200);
+    });
+
+    $(".goose").hover(function(){
+        $(".hover-box4").fadeToggle(200);
+    });
+
+    $(".bannana").hover(function(){
+        $(".hover-box5").fadeToggle(200);
+    });
+
+    $(".dog").hover(function(){
+        $(".hover-box6").fadeToggle(200);
+    });
+
+    $(".hair").hover(function(){
+        $(".hover-box7").fadeToggle(200);
+    });
+
+    $(".person").hover(function(){
+        $(".hover-box8").fadeToggle(200);
+    });
+
+});
